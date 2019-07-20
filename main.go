f45b22f19672a35dbbcdb4a7470521f717cbbaae
mino <lv.mitsuru@gmail.com>
2019-07-20 18:54:42 +0900

    modified main.go

--- main.go
+++ main.go
@@ -3,32 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+
+	"github.com/mikunup/test/fizzbuzz"
 )
 
 func main() {
-	r, err := fizzBuzz(12)
+	r, err := fizzbuzz.FizzBuzz(15)
 
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-	s := string(r)
-	fmt.Printf(s)
-}
-
-func fizzBuzz(n int) (string, error) {
-	if n < 1 || n > 100 {
-		return "", fmt.Errorf("invalid number", n)
-	}
-
-	switch {
-	case n%15 == 0:
-		return "FizzBuzz", nil
-	case n%3 == 0:
-		return "Fizz", nil
-	case n%5 == 0:
-		return "Buzz", nil
-	default:
-		return fmt.Sprint(n), nil
-	}
+	fmt.Printf(r)
 }
